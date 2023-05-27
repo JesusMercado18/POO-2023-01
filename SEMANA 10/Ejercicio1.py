d2a70f504fab2c90da5ba58465b274559862656b
JesusMercado18 <51756960+JesusMercado18@users.noreply.github.com>
2023-05-27 14:56:03 -0500

    Create Ejercicio1.py

---
+++ SEMANA 10/Ejercicio1.py
@@ -0,0 +1,22 @@
+#REALIZANDO FUNCIONES
+def hallarFactorial(i):
+    fact = 1 #LE ASIGNO 1 PORQUE EL FACTORIAL DEBE COMENZAR A MULTIPLICAR DESDE 1
+    for j in range(1, i + 1):
+        fact = fact * j
+    return fact
+
+#DEFINIENDO VARIABLES
+n = 0
+e = 0.0
+factorial = 0.0
+
+#VALIDANDO DATOS
+while n < 1 or n > 121:
+    n = int(input("Ingrese valor de N: "))
+
+#RESOLVIENDO
+for i in range(1, n+1):
+    factorial = hallarFactorial(i)
+    e += (i**2) / (2*factorial)
+
+print(f"El valor del numero e es: {round(e,5)}")
